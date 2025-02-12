6db7ed35c58fbe9d8a97e3d88fa6f0277677a708
lucytang313 <sarang@caresanctum.com>
2025-02-12 09:19:16 +0530

    added confirm password when register

--- user_onboarding/serializers.py
+++ user_onboarding/serializers.py
@@ -5,9 +5,10 @@ from .models import *
 CustomUser = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True, min_length=6) #added a confirm password field while registration
     class Meta:
         model = CustomUser
-        fields = ["email", "phone_number", "password"]
+        fields = ["email", "phone_number", "password", "confirm_password"]
         extra_kwargs = {
             "password": {"write_only": True, "min_length": 6},
         }
@@ -15,9 +16,12 @@ class RegisterSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if not data.get("email") and not data.get("phone_number"):
             raise serializers.ValidationError("Either email or phone number is required.")
+        if data["password"] != data["confirm_password"]: #validate confirm_password and password
+            raise serializers.ValidationError({"confirm_password": "Passwords do not match."})
         return data
 
     def create(self, validated_data):
+        validated_data.pop("confirm_password") #remove confirm_password before saving
         user = CustomUser.objects.create_user(**validated_data)
         return user
 
