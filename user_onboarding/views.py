21f7eb175b6a426b140401dcb0caba0568ff86b4
lucytang313 <sarang@caresanctum.com>
2025-02-26 19:12:27 +0530

    Changes for deploying on render

--- user_onboarding/views.py
+++ user_onboarding/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
-import requests
+# import requests
 import uuid
 from django.db import transaction
 from django.shortcuts import get_object_or_404
