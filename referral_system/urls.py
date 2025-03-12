1a7f02c4a93b6804c978cdcb40143838df3bd5a0
careEng <sairamp@caresanctum.com>
2025-03-13 01:02:08 +0530

    B2B dahsboard

--- referral_system/urls.py
+++ referral_system/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("b2c-code/", send_b2c_code, name="get-b2c-code"),
-    path("b2b-code/", send_b2b_code, name="get-b2b-code"),
+    path("b2b-code/", send_b2b_details, name="get-b2b-code"),
     path("create-lead/", create_lead, name="create-lead"),
     path("create-commission/", create_commission, name="create-commission"),
     path("get-leads/", get_leads_and_commissions, name="get-leads"),
